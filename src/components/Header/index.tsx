60c82ef642a0f5dc42a913383bef17208e3d77ab
Jose Airton <riosjunior270@gmail.com>
2021-02-22 10:31:52 -0300

    Correção 5

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,11 +13,29 @@ import {
     UserImage,
 } from '../../styles/HeaderStyle';
 
+import {
+    SectionContainer,
+    SignIn,
+    Title,
+    Form,
+    Label,
+    Input,
+    Right,
+    LinkSec,
+    Button,
+    BoxButton,
+    Card,
+    InsideCard,
+    OutsideCard,
+    Paragraph
+} from '../../styles/SectionStyle';
+
 export default function Header(){
     const router = useRouter();
     
 
     return (
+        <>
     
             <HeaderContainer>
                 <OutLogo>
@@ -56,5 +74,49 @@ export default function Header(){
                 </UserFace>
             </HeaderContainer>
 
+            <SectionContainer>
+            <SignIn>
+                <Title>Crie sua conta</Title>
+                <Form method='POST'>
+                    <Label>Nome:</Label>
+                    <Input type='text' placeholder='Digite seu nome'/>
+
+                    <Label>Email:</Label>
+                    <Input type='email' placeholder='Digite seu email'/>
+
+                    <Label>Senha:</Label>
+                    <Input type='password' placeholder='Digite sua senha'/>
+
+                    <LinkSec href='#'>Já possui uma conta?</LinkSec>
+
+                    <BoxButton>
+                    <Button type='submit'>Cadastre-se</Button> 
+                    </BoxButton>
+                    
+
+                </Form>
+            </SignIn>
+
+            <Right>
+                <Card>
+                    <InsideCard>
+                        <Image
+                            src='/imgs/Card.svg'
+                            alt='Card' 
+                            width='356' 
+                            height='186'
+                        />
+                        <Paragraph>
+                        Animações em React Native para implementar sua aplicação
+                        </Paragraph>
+                    </InsideCard>
+                    <OutsideCard>
+                        <Paragraph>Aprenda animações basicas em React Native que tornarão suas aplicações mais chamativas.</Paragraph>
+                    </OutsideCard>
+                </Card>
+            </Right>
+            </SectionContainer>
+
+        </>
     );
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,9 @@
 import Header from '../components/Header';
-import Section from '../components/Section';
 
 export default function SignIn(){
     return(
         <>
            <Header/>
-           <Section/>
         </>
     )
 }
